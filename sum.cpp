527a67ebf7c2928d25c4c1e434dbc5e451e1d9eb
Siddarth Gaikawad <siddarthgaikawad21@gmail.com>
2026-07-21 09:22:41 +0000

    cpp code

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+//using namespace std;
+int main()
+{
+    int num1,num2,sum,avg;
+   std::cout<<"enter two number";
+    std::cin>>num1;
+    std::cin>>num2;
+    sum=num1+num2;
+    avg=sum/2;
+    std::cout<<"sum="<<sum<<"\t average="<<avg;
+    return 0;
+}
