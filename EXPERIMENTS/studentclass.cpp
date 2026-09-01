a9e3a3c1da4d7bd9fb8a721c41b9a8a52757e094
Siddarth Gaikawad <siddarthgaikawad21@gmail.com>
2026-09-01 10:21:10 +0000

    commit

---
+++ EXPERIMENTS/studentclass.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Student
+{
+private:
+    string name;
+    int rollNo;
+    float marks;
+//public:
+    void inputDetails()
+    {
+        cout << "Enter Student Name: ";
+        getline(cin >> ws, name);
+
+        cout << "Enter Roll Number: ";
+        cin >> rollNo;
+
+        cout << "Enter Marks: ";
+        cin >> marks;
+    }
+    public:
+void displayDetails()
+    {
+        inputDetails();
+
+        cout << "\n---- Student Details ----\n";
+        cout << "Name    : " << name << endl;
+        cout << "Roll No.: " << rollNo << endl;
+        cout << "Marks   : " << marks << endl;
+    }
+};
+
+int main()
+{
+    Student s;
+
+    //s.inputDetails();
+    //s.rollNo=100;
+    s.displayDetails();
+    return 0;
+}
+
